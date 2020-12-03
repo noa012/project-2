b05b417052bf6947b6e801147436b98d5aa81f5e
noa012 <74935097+noa012@users.noreply.github.com>
2020-12-03 23:15:11 +0100

    Add files via upload

---
+++ index.vue
@@ -0,0 +1,34 @@
+<template> 
+  <div id="app">
+    <div class="container">
+      <div class="moviesonscreeen">
+          <li v-for="movie of movies" :key="movie.id">{{movie.original_title}}</li>
+      </div>
+      <div>
+        <button @click="fetch">click me</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+let url="https://api.themoviedb.org/3/search/movie?api_key=ddac2a9248a58d23cea75a9609c45bbc&query=jack"
+export default {
+  data(){
+    return{
+      movies:[]
+    }
+  },
+  methods:{
+    async fetch(){
+      this.movies = await axios.get(url).then(res=>res.json).catch((e)=>{console.log(e)})
+      console.log("data:",movies)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
